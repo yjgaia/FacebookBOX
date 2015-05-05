9feeab00ee01ebd2d250e773e25a4f5ead72af94
Hanul <hanul@hanul.me>
2015-05-06 03:28:50 +0900

    add description option to share

--- Facebook/BROWSER/LIB/facebook.js
+++ Facebook/BROWSER/LIB/facebook.js
@@ -13,6 +13,6 @@ document.body.appendChild(fbRoot);
 		js = d.createElement('script');
 		js.id = id;
 		js.async = true;
-		js.src = "//connect.facebook.net/en_US/all.js";
+		js.src = "http://connect.facebook.net/en_US/all.js";
 		ref.parentNode.insertBefore(js, ref);
 	}(document));
--- Facebook/BROWSER/share.js
+++ Facebook/BROWSER/share.js
@@ -3,17 +3,22 @@ Facebook.share = METHOD({
 	run : function(params) {'use strict';
 		//REQUIRED: params
 		//REQUIRED: params.href
+		//OPTIONAL: params.description
 		//OPTIONAL: params.display
 
 		var
 		// href
 		href = params.href,
+		
+		// description
+		description = params.description,
 
 		// display
 		display = params.display;
 
 		FB.ui({
 			method : 'feed',
+			description : description,
 			display : display,
 			link : href
 		}, function(response) {
